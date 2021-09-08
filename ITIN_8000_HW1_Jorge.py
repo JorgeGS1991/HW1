41c1343cd61874b956b141287687659e7a9c139e
Jorge Grimaldo <JorgeGS1991@users.noreply.github.com>
2021-09-08 16:20:44 -0500

    Update ITIN_8000_HW1_Jorge.py
    Fixed grammar errors and asked user to enter the user's name.

--- ITIN_8000_HW1_Jorge.py
+++ ITIN_8000_HW1_Jorge.py
@@ -3,6 +3,9 @@
 # import datatime
 from datetime import datetime
 
+# ask user to type username
+username = input ("Whats your name ")
+
 # get the lines stap fro today
 today = datetime.now()
 # Get months name from todays date
@@ -12,15 +15,15 @@ month_name = today.strftime("%B")
 # Extract the day of the month as a number from today's date
 day_number = today.day
 
-# if the day is equial to 1, 21 or 31 the suffix will be st
+# if the day is equals to 1, 21 or 31 the suffix will be st
 if day_number == 1 or 21 or 31:
     suffix = "st"
 
-# if the day is equial to 2 or 22 the suffix will be nd
+# if the day is equals to 2 or 22 the suffix will be nd
 elif day_number == 2 or 22:
     suffix = "nd"
 
-# if the day is equial to 3 or 23 the suffix will be rd
+# if the day is equals to 3 or 23 the suffix will be rd
 elif day_number == 3 or 23:
     suffix = "rd"
 
@@ -28,8 +31,6 @@ elif day_number == 3 or 23:
 else:
     suffix = "th"
 
-
-print(suffix)
 #extract the year as a number from today's date
 year_number = today.year
 
@@ -41,7 +42,7 @@ else:
     day_type = "odd"
 
 # Print out the Statement including month_name, day_number, year_number, today.month*day)number and day_type.
-print("Hello. Todays Date is", month_name, str(day_number), suffix, "of", str(year_number),
+print("Hello.",username, "Todays Date is", month_name, str(day_number), suffix, "of", str(year_number),
       "The product of the month and day is", today.month * day_number, "which is an", day_type, "number.")
 
 
